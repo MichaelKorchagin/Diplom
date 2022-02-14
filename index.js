d335d8238bc92aac556198b8c8de73f9c0f4752e
Kirill Korchagin <dor7an@gmail.com>
2022-02-14 03:05:09 -0800

    Создание файлов АПИ с датой в названии. Вызов методов по аргументам в консоль.

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ require("@babel/core");
 require("@babel/register");
 const { saveData, readData } = require('./src/data');
 
-saveData().then(console.log("end"));
-readData();
+
+switch(process.argv[2]) {
+  case "saveData":
+    saveData().then(console.log("end"));
+    break;
+  case "readData":
+    readData();
+    break;
+};
--- src/data.js
+++ src/data.js
@@ -1,26 +1,34 @@
 import axios from "axios";
 import fs from "fs";
+import { parametrs } from '../index';
 
 const url5Min = 'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=demo';
-const dataPath = `${process.cwd()}/data`; // Во время работы node запускает process. CWD - возвращает путь к текущей папке. = process.cwd() + "data".
-const pathToData = `${dataPath}/come.json`;
+const dataPath = `${process.cwd()}/data/`; // Во время работы node запускает process. CWD - возвращает путь к текущей папке. = process.cwd() + "data".
 
 export const saveData = async () => {
+  const objDate = new Date();
+  const dateForNameMonth = String(objDate.getMonth() + 1).padStart(2, "0");
+  const dateForNameDay = String(objDate.getDay()).padStart(2, "0");
+  const nameWithDate = dateForNameMonth + "-" +  dateForNameDay;
+  const pathToData = dataPath + nameWithDate + ".json";
+
   const data = await axios.get(url5Min);
   const dataString = JSON.stringify(data.data, null, "\t");  // Переводит Джсон в стринг
   fs.writeFileSync(pathToData, dataString); // записывает в файл полный документ (путь, data)
 };
 
-export const readData = () => {
-  const fileString = fs.readFileSync(pathToData); // возвращает весь файл, как стрингу
+const dateToRead = process.argv[3];
+export const readData = (dateToRead) => {
+  const pathToDataHere = dateToRead + ".json";
+  const fileString = fs.readFileSync(pathToDataHere); // возвращает весь файл, как стрингу
   const apiFile = JSON.parse(fileString);
-  const upperKey = "Time Series Crypto (5min)";   // заменяет вызов через точку, потому что стринга
-  const lowerKey = "2022-01-14 08:30:00";
-  const value = apiFile[upperKey][lowerKey];
+  const upperKey = "Time Series Crypto (5min)";   // заменяют вызов через точку, потому что стринга
+  // const lowerKey = "2022-01-14 08:30:00";
+  const value = apiFile[upperKey];
 
   console.log(value);
 };
 
-// TODO: 1. Запускать отдельно все функции по агрументам в консоль.
-//       2. SaveData каждый раз в новый файл.
-//       3. Реализовать выбор ключей и файла Даты по агрументам в консоли.
+// TODO: 3. Реализовать выбор ключей и файла Даты по агрументам в консоли ???
+//       4. Настроить правильные даты в названиях файлов.
+//       5. Получать графики во всем временным интервалам.
