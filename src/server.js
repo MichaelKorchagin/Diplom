661667b0b760efb0aea0b8ec1dfd6fbadf1343fa
Michael Korchagin <benderrodriguez932@gmail.com>
2022-06-11 16:39:25 +0300

    Моделирование графика: 1. Заходит в html 2. Делает запрос (/data) в сервер 3. Возвращает JSON 4. И засунул в dataArray для отрисовки лиbой.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,48 @@
 import http from 'http';
 import { modelData } from './data';
+import fs from "fs";
 
-const port = 3001;
+const port = 8080;
 
 console.log(`Server was started at port: ${ port }`);
 
-http.createServer(function (request, response) {
+const server = http.createServer(async function (request, response) {
 
-  if (request.url === '/index') {
-    console.log('stepzzz');
-    response.end(JSON.stringify(modelData("03-01", "5")));
+    console.log(request.url);
+    switch (request.url) {
+      case "/index":
+        fs.readFile('./index.html', function (err, data) {
+          response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+          response.write(data);
+          response.end();
+        });
+        // let dataIndex = new Bvvuffer();
+        // await fs.readFile('./index.html', function(err, content) {
+        //   dataIndex = content;
+        // });
+        // response.writeHead(200, { 'Content-Type': 'text/html' });
+        // response.write(dataIndex);
+        // response.end();
+        break;
+
+      case "/styles.css":
+        fs.readFile('./styles.css', function (err, data) {
+          response.writeHead(200, { 'Content-Type': 'style/css', 'Content-Length': data.length });
+          response.write(data);
+          response.end();
+        });
+        break;
+
+      case "/data":
+        response.writeHead(200, { 'Content-Type': 'application / json' });
+        console.log('stepzzz');
+        response.write(JSON.stringify(modelData("05-26", "5")))
+        response.end();
+        break;
+
+      default:
+        console.log('Hello');
+        response.end();
+    }
   }
-  else {
-    response.end('Hello');
-  }
-}).listen(port);
+).listen(port);
